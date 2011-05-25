9b188800dac49e23e3bd4cd546a5da8d9a419c1b
Claudio Mignanti <c.mignanti@gmail.com>
2011-05-25 16:47:23 +0200

    cnc: align the python script to the kernel driver

--- cnc.py
+++ cnc.py
@@ -1,32 +1,31 @@
 # This file is released under the term of GPLv2.
 #
-# 2010 (R) - Claudio Mignanti  <c.mignanti@gmail.com>
+# 2010-2011 (R) - Claudio Mignanti  <c.mignanti@gmail.com>
 #
-# $Id$
 
-import fox
 from threading import Thread
 
 class axis(Thread):
-	stepper		 = 0 # class
+	stepper		 = 0 # device fd
 	miss_step	 = 0 # int
 	act_position = 0 # mm
-	unit = 0 		# 0= mm 1= inches
-	max_step = 0	# int
-	step_per_mm = 0	# int
+	unit = 0 	 # 0= mm 1= inches
+	max_step = 0	 # int
+	step_per_mm = 0	 # int
 
-	position_to_go = 0
 	home	= 0		# mm
 	end_dir	= 0
 
 
-	def __init__ (self, stepper_class, step_max, step_per_mm=315, unit=1):
-		""" This method is a shell of stepper one. Mainly it is used to manage the axis limit and threading the operation!"""
+	def __init__ (self, stepper_dev, step_max, step_per_mm=315, unit=0):
+		""" This method is a shell of stepper motor. Mainly it is used to manage the axis limit and threading the operation!"""
 		Thread.__init__(self)
-		self.stepper	 = stepper_class
+		self.stepper = open (stepper_dev, "rw")
 		self.max_step	 = step_max
 		self.step_per_mm = step_per_mm
 		self.unit = unit
+
+		self.stepper.ioctl(MOTOR_LOWPWR)
 		return None
 
 #axis diagram
@@ -41,7 +40,7 @@ class axis(Thread):
 	def run (self):
 		#calcolate steps and direction from position
 		diff = self.position_to_go - self.act_position
-		if (diff < 0):
+		if diff < 0:
 			direction = 0
 			diff = diff * -1
 		else:
@@ -53,16 +52,18 @@ class axis(Thread):
 			num_step = diff * step_per_mm
 
 		#check if we can move in the opposide direction
-		if (self.is_at_end == 1):
-			if (direction == self.end_dir ):
+		if self.is_at_end == 1:
+			if direction == self.end_dir:
 				#yet at the end here on this direction
 				return 1
 
-		self.stepper.dir(direction)
-		self.stepper.enable()
-		self.miss_step = self.stepper.step(num_step, 0, 7)
-		if (miss_step != 0):
-			# we are at the end of axis
+		self.stepper.ioctl(MOTOR_ENABLE)
+		self.stepper.ioctl(MOTOR_DIR, direction)
+
+		miss_step = self.stepper.write(str(position_to_go))
+		if miss_step != 0:
+			print "End of axis, missed step %d" % miss_step
+			# we are at the end of axis 
 			self.is_at_end 	= 1
 			self.end_dir 	= direction
 
@@ -71,9 +72,14 @@ class axis(Thread):
 			self.act_position = position - ( miss_step * ( step_per_mm * 2.54 ))
 		else:
 			self.act_position = position - ( miss_step * step_per_mm)
+
+		self.stepper.ioctl(MOTOR_DISABLE)
 		return 0
 
-	#run Thread cannot get 2 parameters
+	def pwm_set(self, val)
+		self.stepper.ioctl(MOTOR_PWM_SET, val)
+
+	#run of Thread cannot get 2 parameters
 	def set_position(self, position):
 		self.position_to_go = position
 
@@ -89,10 +95,19 @@ class axis(Thread):
 	def get_home(self):
 		return self.home
 
+	def go_home(self):
+		self.position_to_go = self.home
+		self.start()
+
+	def unit_per_sec(self,val):
+		self.pwm_set(1/(step_per_mm * val * 2))
+
+	def __del__ (self):
+		stepper.close()
+
 
 class cnc_3A0T():
-	""" This method can be used to manage a complete 3 axes cnc without tool.\n
-You should add 3 existing axes to it to operate."""
+	""" This method can be used to manage a complete 3 axes cnc without tool.\n"""
 
 	if_error = 0
 
@@ -115,14 +130,24 @@ You should add 3 existing axes to it to operate."""
 			ax.join()
 
 	def reset(self):
-		self.z_axis.start(-99999)
+		#leave tool in home position before move x/y
+		self.z_axis.go_home()
 		self.z_axis.join()
-		self.x_axis.start(-99999)
-		self.y_axis.start(-99999)
+
+		self.x_axis.go_home()
+		self.y_axis.go_home()
 		self.x_axis.join()
-		self.y_axis.join
+		self.y_axis.join()
 
 	def command(self, cmd):
 		
+
 		return 0
+
+	def g_file_execute(self, filename):
+		fd = open(filename)
+
+		self.command(fd.readline())
+
+		
 
--- gcode.py
+++ gcode.py
@@ -1,44 +1,49 @@
 class gcode_interpreter:
 
-			def interpreter(self):
-				status = 0			#I implement the gcode interpreter like 
-				#	a status machine. eache command is a state
+	def __init__(self):
+		status = 0			#I implement the gcode interpreter like 
+		#	a status machine. each command is a state
 
-				#list of basic commands from arduino firmware for reprap
-			l = cmd.partition("GTM");
+	#list of basic commands from arduino firmware for reprap
+		l = cmd.partition("GTM");
 
-			if (l[0] == "G00"): #rapid motion
-					pass
-			if (l[0] ==  "G01"): #coordinated motion
-					pass
-			if (l[0] == "G02"): #arc, clockwise
-				pass
-			if (l[0] == "G03"): #arc counter clockwise
-				pass
-			if (l[0] == "G04"): #dwell
-				pass
+		if (l[0] == "G00"): #rapid motion
+			x.set_position(l[1])
+			x.start()
+			y.set_position(l[2])
+			y.start()
 
-			if (l[0] == "G20"): #inches as units
-				for ax in self.x_list:
-						ax.unit("inches")
 
-			if (l[0] == "G21"): #millimeters as units
-				for ax in self.x_list:
-						ax.unit("mm")
+		if (l[0] ==  "G01"): #coordinated motion
+			pass
+		if (l[0] == "G02"): #arc, clockwise
+			pass
+		if (l[0] == "G03"): #arc counter clockwise
+			pass
+		if (l[0] == "G04"): #dwell
+			pass
 
-			if (l[0] == "G28"): #go home
-				for ax in self.x_list:
-						ax.start(ax.get_home(), 0)
-				wait_axis()
+		if (l[0] == "G20"): #inches as units
+			for ax in self.x_list:
+				ax.unit("inches")
 
-			if (l[0] == "G30"): #go home via intermediate point
-				pass
+		if (l[0] == "G21"): #millimeters as units
+			for ax in self.x_list:
+				ax.unit("mm")
 
-			if (l[0] == "G91"): #incremental positioning
-				pass
+		if (l[0] == "G28"): #go home
+			for ax in self.x_list:
+				ax.go_home()
+			wait_axis()
 
-			if (l[0] == "G92"): #set current as home
-				for ax in self.x_list:
-						ax.set_home()
+		if (l[0] == "G30"): #go home via intermediate point
+			pass
+
+		if (l[0] == "G91"): #incremental positioning
+			pass
+
+		if (l[0] == "G92"): #set current as home
+			for ax in self.x_list:
+				ax.set_home()
 
 
